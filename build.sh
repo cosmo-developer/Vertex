d139d9f8f2e03d5b94f9445c7e07e5640900a284
Sonu Aryan <57495903+cosmo-developer@users.noreply.github.com>
2020-10-04 11:43:58 -0400

    build system

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 Vertex.cpp Lexer.cpp InstanceManager.cpp ErrorHandler.cpp Token.cpp Interpretor.cpp -o Vertex
+g++ -std=c++11 Vertex.cpp Lexer.cpp InstanceManager.cpp ErrorHandler.cpp Token.cpp Interpretor.cpp VertexObject.cpp -o Vertex
